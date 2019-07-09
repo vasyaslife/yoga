640ed69dc77c64a1c39e5a802d93a960a32aec25
Vasiliy <vasyasjob@gmail.com>
2019-07-10 00:45:47 +0300

    added modal show and hide by taping on descr btn and more btn

--- js/main.js
+++ js/main.js
@@ -90,4 +90,30 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Put this when you need to init your timer
     setClock('timer', deadline);
+
+    // Modal
+    let moreBtn = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        closeBtn = document.querySelector('.popup-close'),
+        descrBtn = document.querySelectorAll('.description-btn');
+
+    moreBtn.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    descrBtn.forEach(function(item) {
+        item.addEventListener('click', function() {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    closeBtn.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        moreBtn.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
 });
