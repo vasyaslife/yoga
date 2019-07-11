fb23b5d73a700bd78a077adbef8057c5818f90a3
Vasiliy <vasyasjob@gmail.com>
2019-07-12 00:12:01 +0300

    added calc func

--- js/main.js
+++ js/main.js
@@ -151,7 +151,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     contactForm.addEventListener('submit', (event) => {
         event.preventDefault();
-        sendData(form, input)
+        sendData(contactForm, contactInput)
                 .then(() => {
                     statusMsg.innerHTML = message.loading;
                 })
@@ -249,4 +249,50 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         }
     });
+
+    // Calc
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+
+        persons.addEventListener('change', function() {
+            personsSum = +this.value;
+            total = (daysSum + personsSum) * 5000;
+
+            if (restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });
+
+        restDays.addEventListener('change', function() {
+            daysSum = +this.value;
+            total = (daysSum + personsSum) * 5000;
+
+            if (restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });
+
+        place.addEventListener('change', function() {
+            if (restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+        });
 });
+
+
+
+
